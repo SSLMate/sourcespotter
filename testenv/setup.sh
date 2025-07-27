2b061a4465d7cebc942bdb799465a6d7aba85b94
Andrew Ayer <agwa@andrewayer.name>
2025-07-26 22:44:04 -0400

    Fix testenv

--- testenv/setup.sh
+++ testenv/setup.sh
@@ -11,23 +11,13 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y postgresql mkcert >/dev/null
 # Start PostgreSQL
 service postgresql start
 
-# Create database and user if not exist
+# Initialize database
 sudo -u postgres psql <<'PSQL'
-DO $$
-BEGIN
-   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'sourcespotter') THEN
-      CREATE ROLE sourcespotter LOGIN PASSWORD 'sourcespotter';
-   END IF;
-END$$;
+CREATE ROLE sourcespotter LOGIN PASSWORD 'sourcespotter';
 CREATE DATABASE sourcespotter OWNER sourcespotter;
-PSQL
-
-# Load schema
-sudo -u postgres psql -d sourcespotter -f schema/20-tables.sql
-sudo -u postgres psql -d sourcespotter -f schema/30-triggers.sql
-
-# Populate dummy data
-sudo -u postgres psql -d sourcespotter <<'PSQL'
+SET ROLE sourcespotter;
+\i schema/20-tables.sql
+\i schema/30-triggers.sql
 \copy db from 'testenv/testdata/db'
 \copy sth from 'testenv/testdata/sth'
 \copy record from 'testenv/testdata/record'
@@ -38,7 +28,7 @@ PSQL
 # Generate TLS certificate
 mkcert -install >/dev/null
 mkcert -cert-file testenv/cert.pem -key-file testenv/key.pem \
-       sourcespotter.localhost *.api.sourcespotter.localhost
+       sourcespotter.localhost '*.api.sourcespotter.localhost'
 
 # Combine cert and key because go-listener expects them in one file
 cat testenv/key.pem testenv/cert.pem > testenv/server.pem
