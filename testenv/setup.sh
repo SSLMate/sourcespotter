8c0ac196c7c5a34004919c605afcffeaf56c38eb
Andrew Ayer <agwa@andrewayer.name>
2025-07-27 09:44:53 -0400

    Improve testenv

--- testenv/setup.sh
+++ testenv/setup.sh
@@ -6,13 +6,15 @@ cd "$REPO_ROOT"
 
 # Install dependencies
 apt-get update || true
-DEBIAN_FRONTEND=noninteractive apt-get install -y postgresql mkcert >/dev/null
+DEBIAN_FRONTEND=noninteractive apt-get install -y postgresql mkcert
 
 # Start PostgreSQL
 service postgresql start
 
 # Initialize database
 sudo -u postgres psql <<'PSQL'
+DROP DATABASE IF EXISTS sourcespotter WITH (force);
+DROP ROLE IF EXISTS sourcespotter;
 CREATE ROLE sourcespotter LOGIN PASSWORD 'sourcespotter';
 CREATE DATABASE sourcespotter OWNER sourcespotter;
 \c sourcespotter
@@ -24,13 +26,15 @@ SET ROLE sourcespotter;
 \copy record from 'testenv/testdata/record'
 \copy toolchain_source from 'testenv/testdata/toolchain_source'
 \copy toolchain_build from 'testenv/testdata/toolchain_build'
+SELECT setval('db_db_id_seq', (SELECT MAX(db_id) FROM db), true);
+SELECT setval('sth_sth_id_seq', (SELECT MAX(sth_id) FROM sth), true);
 PSQL
 
 # Generate TLS certificate
-mkcert -install >/dev/null
-mkcert -cert-file testenv/cert.pem -key-file testenv/key.pem \
+mkcert -install
+mkcert -cert-file /etc/sourcespotter-certonly.pem -key-file /etc/sourcespotter-key.pem \
        sourcespotter.localhost '*.api.sourcespotter.localhost'
 
 # Combine cert and key because go-listener expects them in one file
-cat testenv/key.pem testenv/cert.pem > testenv/server.pem
-chmod 644 testenv/server.pem
+cat /etc/sourcespotter-key.pem /etc/sourcespotter-certonly.pem > /etc/sourcespotter-cert.pem
+chmod 644 /etc/sourcespotter-cert.pem
