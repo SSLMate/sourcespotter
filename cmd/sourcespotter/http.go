bfa554f8689853b24278183d35714c8c640c886f
Andrew Ayer <agwa@andrewayer.name>
2025-11-20 11:18:00 -0500

    Remove modcheck API

--- cmd/sourcespotter/http.go
+++ cmd/sourcespotter/http.go
@@ -68,8 +68,6 @@ func newHTTPServer() *http.Server {
 	mux.HandleFunc("POST gossip.api."+domain+"/{address}", sths.ReceiveGossip)
 	// v1 public API
 	mux.HandleFunc("POST v1.api."+domain+"/modules/authorized", modules.ReceiveAuthorized)
-	// private API
-	//mux.HandleFunc("GET private.api."+domain+"/modcheck", modcheck.Serve)
 
 	return &http.Server{
 		ReadTimeout:  5 * time.Second,
